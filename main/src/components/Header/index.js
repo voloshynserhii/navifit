1e115e6436cd3914f035a3d66af15d8600e9f85a
voloshynserhii <voloshynserhii1982@gmail.com>
2024-01-19 16:09:14 +0100

    create icons for header

--- main/src/components/Header/index.js
+++ main/src/components/Header/index.js
@@ -1,11 +1,18 @@
-import styles from './page.module.css'
+import MenuIcon from '../Icons/Menu';
+import HelpIcon from '../Icons/Help';
+import styles from './page.module.css';
 
 const Header = () => {
     return (
         <div className={styles.header}>
             <p>NAVIFIT</p>
-            <p>MENU</p>
+            <div className={styles.iconsGroup}>
+                <HelpIcon />
+                <MenuIcon />
+            </div>
+
         </div>
     )
-}
+};
+
 export default Header;
---
+++ main/src/components/Icons/Menu.js
@@ -0,0 +1,12 @@
+import styles from './icons.module.css';
+
+export default function Menu({ fillColor }) {
+    return (
+        <div className={styles.icon}>
+            <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M4.41455 11.7712V10.2998H29.4288V11.7712H4.41455ZM29.4288 17.6569V19.1284H9V17.6569H29.4288ZM4.41455 25.0141H29.4288V26.4855H4.41455V25.0141Z" fill="#CCFF33" />
+                <path d="M4.41455 11.7712V10.2998H29.4288V11.7712H4.41455ZM29.4288 17.6569V19.1284H9V17.6569H29.4288ZM4.41455 25.0141H29.4288V26.4855H4.41455V25.0141Z" fill="black" fill-opacity="0.2" />
+            </svg>
+        </div>
+    );
+};

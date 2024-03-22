891f002b3deb4da6b17d657fe427bf21452bd72d
voloshynserhii <voloshynserhii1982@gmail.com>
2024-03-22 12:41:25 +0100

    global font size remove

--- main/src/components/UserPermission/index.js
+++ main/src/components/UserPermission/index.js
@@ -4,11 +4,14 @@ import { styled } from '@mui/material/styles';
 const StyledSpan = styled(Typography)(({ theme }) => ({
     color: theme.palette.secondary.contrastText,
     cursor: 'pointer',
+    [theme.breakpoints.down("md")]: {
+        fontSize: '12px',
+      },
 }))
 
 const UserPermission = () => {
     return (
-        <Typography variant="body16" sx={{ color: 'primary.grey2', lineHeight: { xs: '18px', md: 'inherit' } }}>
+        <Typography variant="body16" sx={{ color: 'primary.grey2', fontSize: { xs: '12px', md: 'inherit' }, lineHeight: { xs: '18px', md: 'inherit' } }}>
             Wybierając cel i kontynuując, wyrażasz zgodę na nasze
             <StyledSpan component='span' variant='span'> Warunki korzystania</StyledSpan> z usługi | <StyledSpan component='span' variant='span'>Polityka prywatności</StyledSpan>
         </Typography>
